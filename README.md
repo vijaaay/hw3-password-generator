2b756e66440ae0a03b5f7466fe5b16cf354c07a4
Vijay Suvarnan <vijaysuvarnan@gmail.com>
2020-10-16 23:30:29 -0700

    Commiting all files for the project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# hw3-password-generator
+# Homework: Code Refactor
+https://github.com/vijaaay/hw1-code-refactor/blob/main/code-refactor-screenshot.png
+
+# Site
+https://vijaaay.github.io/hw3-password-generator/
+
+## Description
+This homework assignment asked us to create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code.
+
---
+++ script.js
@@ -0,0 +1,32 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//Character type arrays
+var uppercaseAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseAlphabet=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specChar = [ "!", "#", "$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+var number = [0,1,2,3,4,5,6,7,8,9]
+
+
+//Criteria prompt for password
+//Length prompt for password
+//Character types to include in password
+//Choose lowercase, uppercase, numeric, and/or special characters
+//Input validation and at least one of each type should be selected
+//The password is generated using selected criteria
+//Password displayed
+
+
+
